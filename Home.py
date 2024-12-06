7f5325df905fbf7c3224511c18e321a8ec9d92cb
Wambui Mwangi <mwangi25.mercy@gmail.com>
2024-12-06 16:17:04 +0300

    Fixing relative path

--- Home.py
+++ Home.py
@@ -14,7 +14,7 @@ st.write("""
 
          **Simply upload a satellite image and get accurate forest cover maps in just a click of a button!**
          """)
-landing_page_image=Image.open(r"C:\Users\Pula Advisors\Desktop\Image-Segmentation-for-Forest-Cover-Mapping\Results\forest_cover.jpeg")
+landing_page_image=Image.open("Results\forest_cover.jpeg")
 
 st.image(landing_page_image,caption=None,width=800)
 st.sidebar.success("Navigate App here")
